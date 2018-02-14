2c0f9218116d96435b81e2295bb8f1014828640e
Eduard Schlotter <eduardschlotter@gmail.com>
2018-02-14 14:38:10 +0000

    Light changes

--- pi/Actuator.py
+++ pi/Actuator.py
@@ -17,21 +17,27 @@ def scan(message, expected_reply, set_port=None, type=None, mac=None):
             if 'tcp' in s["scan"][ip]:
                 if 'mac' in s["scan"][ip]['addresses'] and mac == None:
                     print(s["scan"][ip]['addresses']['mac'])
-                    mac = s["scan"][ip]['addresses']['mac']
+                    mac = s["scan"][ip]['addresses']['mac'] # get mac address
+
+                if mac.startswith("AC:CF:23"):
+                    # Light manufacturer mac address found
+                    actuators[mac] = {}
+                    actuators[mac]['type'] = type
+                    actuators[mac]['ip'] = ip
+                    actuators[mac]['mac'] = mac
 
                 for port in s["scan"][ip]["tcp"]:
-                    if (set_port == port or set_port == None):
+                    if set_port == port or set_port == None and message != None:
                         print (port)
                         if verify_connection(ip, port, message, expected_reply):
-                            print('hh')
+                            # Saving
                             actuators[mac] = {}
                             actuators[mac]['type'] = type
                             actuators[mac]['ip'] = ip
                             actuators[mac]['mac'] = mac
                             print ("Added" + ip + "/" + str(port))
                             actuators.append(ip + "/" + str(port) + "-" + mac)
-                            # ignore 80 and 22 + self ?
-                mac = old_mac
+                mac = old_mac # overwrite mac
                 print("\n")
             else:
                 print("TCP Failed")
@@ -60,8 +66,8 @@ def send_message(ip, port, message):
         s.connect((ip, port))
         s.sendall(message)
         s.close()
-    except:
-        print ("Conection Failed")
+    except Exception as e:
+        print (e)
 
 # ---- Kettle Functions ----
 def find_kettle():
@@ -87,32 +93,32 @@ def set_kettle_warm(ip, port):
 
 
 # ---- Kettle Functions ----
-def find_light(mac):
-    scan("APP#"+mac+"#CMD#420\n", "OK#sent", 38899, "Light", mac) # response may be wrong
+def find_light():
+    scan(None, None, None, "Light") 
 
-def turn_all_lights_on(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#420\n")
+def turn_all_lights_on(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#420\n")
 
-def turn_all_lights_off(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#410\n")
+def turn_all_lights_off(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#410\n")
 
-def turn_group_1_on(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#450\n")
+def turn_group_1_on(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#450\n")
 
-def turn_group_1_off(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#460\n")
+def turn_group_1_off(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#460\n")
 
-def turn_group_2_on(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#470\n")
+def turn_group_2_on(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#470\n")
 
-def turn_group_2_off(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#480\n")
+def turn_group_2_off(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#480\n")
 
-def turn_group_3_on(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#490\n")
+def turn_group_3_on(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#490\n")
 
-def turn_group_3_off(ip, port, mac):
-    send_message(ip, port, "APP#" + mac + "#CMD#4a0\n")
+def turn_group_3_off(mac):
+    send_message("178.62.58.245", 38899, "APP#" + mac + "#CMD#4a0\n")
 #APPAC:CF:23:28:C2:2C
 #OKAC:CF:23:28:C2:2C
 
@@ -120,5 +126,6 @@ def turn_group_3_off(ip, port, mac):
 if __name__ == '__main__':
     print("Start")
     # find_kettle()
-    find_light("AC:CF:23:A1:FB:38")
+    #find_light()
+    turn_group_1_off("AC:CF:23:A1:FB:38")
     print(actuators)
